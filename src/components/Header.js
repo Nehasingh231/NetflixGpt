9ab0854be5884ebadd50a8ad56a0026a497a6ebd
Nehasingh231 <nehakumari14072001@gmail.com>
2024-07-28 03:29:02 +0530

    movies api fetched

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "../utils/firebase";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
@@ -12,50 +12,64 @@ const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const [showSignOut, setShowSignOut] = useState(false);
 
   const handleSignOut = () => {
     signOut(auth)
-      .then(() => {
-      })
+      .then(() => {})
       .catch((error) => {
         navigate("/error");
       });
   };
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        //  Sign IN
+        // Sign In
         const { uid, email, displayName, photoURL } = user;
         dispatch(
           addUser({
-            uid: uid,
-            email: email,
-            displayName: displayName,
-            photoURL: photoURL
+            uid,
+            email,
+            displayName,
+            photoURL
           })
         );
-          navigate("/browse")
+        navigate("/browse");
       } else {
         // Sign Out
         dispatch(removeUser());
-        navigate("/") 
+        navigate("/");
       }
     });
-       return () => unsubscribe();
-  }, []);
+    return () => unsubscribe();
+  }, [dispatch, navigate]);
 
+  const toggleSignOut = () => {
+    setShowSignOut((prevState) => !prevState);
+  };
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between ">
-      <img
-        className="w-44"
-        src={LOGO}
-        alt="logo"
-      />
+      <img className="w-44" src={LOGO} alt="logo" />
       {user && (
-        <div className="flex p-2">
-          <img className="w-20 h-20 rounded-full" src={user?.photoURL} alt="usericon" />
-          <button onClick={handleSignOut}>sign out</button>
+        <div className="relative flex items-center">
+          <img
+            className="w-20 h-20 rounded-full"
+            src={user.photoURL}
+            alt="usericon"
+          />
+          <button onClick={toggleSignOut} className="ml-2">
+          <span><i class="fa-solid fa-angle-down"></i></span>  
+           {/* Downward arrow */}
+          </button>
+          {showSignOut && (
+            <div className="absolute top-24 right-0 bg-white border rounded shadow-md">
+              <button onClick={handleSignOut} className="px-4 py-2 text-black">
+                Sign Out
+              </button>
+            </div>
+          )}
         </div>
       )}
     </div>
